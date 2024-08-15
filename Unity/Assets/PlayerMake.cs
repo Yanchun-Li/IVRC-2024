a8a989f32ffadb53d21f64e88ea7c088fef535c0
Kotaro514 <koutaro.km0514@gmail.com>
2024-08-15 17:24:46 +0900

    Add photon and make room

---
+++ Unity/Assets/PlayerMake.cs
@@ -0,0 +1,28 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using System.Collections.Generic;
+
+ public class PlayerMake : MonoBehaviourPunCallbacks
+ {
+     private void Start() {
+        // プレイヤー自身の名前を"Player"に設定する
+        PhotonNetwork.NickName = "Player";
+
+         PhotonNetwork.ConnectUsingSettings();
+     }
+
+     public override void OnConnectedToMaster() {
+         PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+         //PhotonNetwork.CreateRoom(null, new RoomOptions(), TypedLobby.Default);
+     }
+
+     public override void OnJoinedRoom() {
+         var position = new Vector3(0,0,0);
+         var playerlist = new List<Player>(PhotonNetwork.PlayerList);
+         if (playerlist.Count==2){
+            position = new Vector3(200,0,0);
+         }
+         PhotonNetwork.Instantiate("Avatar", position, Quaternion.identity);
+     }
+ }
