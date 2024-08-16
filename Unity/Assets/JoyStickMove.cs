cdfbffc2c877a9a68cdcbfea8690d20cc4455125
Kotaro514 <koutaro.km0514@gmail.com>
2024-08-16 10:52:28 +0900

    add some script (check Slack)

--- Unity/Assets/JoyStickMove.cs
+++ Unity/Assets/JoyStickMove.cs
@@ -14,10 +14,10 @@ public class JoyStickMove : MonoBehaviour
     {
         //右ジョイスティックの情報取得
         Vector2 stickR = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
-        Vector3 changePosition = new Vector3((0.1f*stickR.x), 0, (0.1f*stickR.y));
+        Vector3 changePosition = new Vector3((0.05f*stickR.x), 0, (0.05f*stickR.y));
         //HMDのY軸の角度取得
         Vector3 changeRotation = new Vector3(0, InputTracking.GetLocalRotation(XRNode.Head).eulerAngles.y, 0);
         //OVRCameraRigの位置変更
-        this.transform.position += this.transform.rotation * (Quaternion.Euler(changeRotation) * changePosition);
+        this.transform.position -= this.transform.rotation * (Quaternion.Euler(changeRotation) * changePosition);
     }
 }
