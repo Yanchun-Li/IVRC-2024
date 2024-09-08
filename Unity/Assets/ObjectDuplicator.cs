1cd317e746c3db5db6149173fe70d4017ab7292d
Kotaro514 <koutaro.km0514@gmail.com>
2024-09-08 19:52:50 +0900

    Update ObjectDuplicator.cs

--- Unity/Assets/ObjectDuplicator.cs
+++ Unity/Assets/ObjectDuplicator.cs
@@ -114,6 +114,11 @@ public class ObjectDuplicator : MonoBehaviour
             {
                 UpdateTransform(originalChild, duplicateChild);
                 UpdateComponents(originalChild.gameObject, duplicateChild.gameObject);
+
+                if (!duplicateChild.gameObject.activeSelf)
+                {
+                    originalChild.gameObject.SetActive(false);
+                }
             }
 
             // 子オブジェクトの位置、回転、スケールを更新
@@ -137,7 +142,6 @@ public class ObjectDuplicator : MonoBehaviour
         original.localPosition = duplicate.localPosition - difforigin;
         original.localRotation = duplicate.localRotation;
         original.localScale = duplicate.localScale;
-        original.gameObject.SetActive(duplicate.gameObject.activeSelf);
     }
 
     private void UpdateComponents(GameObject original, GameObject duplicate)
