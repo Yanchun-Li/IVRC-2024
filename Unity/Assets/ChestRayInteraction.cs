127ebf4f19d86f897a6f4b288ef3a29b8e747a60
Yanchun-Li <liyanchun2001@gmail.com>
2024-12-01 13:51:47 +0900

    update

--- Unity/Assets/ChestRayInteraction.cs
+++ Unity/Assets/ChestRayInteraction.cs
@@ -97,7 +97,7 @@ public class ChestRayInteraction : MonoBehaviourPunCallbacks
         }else if (140-9<position.x  & position.x<140+9 & -60-9<position.z & position.z<-60+9){
             scoreIncrement = 2;
         }else{
-            scoreIncrement = 1;
+            scoreIncrement = 2;
         }
         // スコアを加算
         myscore += scoreIncrement;
